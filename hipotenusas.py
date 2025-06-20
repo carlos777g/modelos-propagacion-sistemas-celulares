014495e10d586ee10a0ed5a36036dadeb055ed70
Carlos Guillen <gcarlosjael@gmail.com>
2025-06-20 11:52:33 -0600

    Agregando distancia real entre BS y cada móvil

--- hipotenusas.py
+++ hipotenusas.py
@@ -4,15 +4,12 @@ import numpy as np
 hipotenusa = []
 with open("datos.json", 'r') as f:
         data = json.load(f)
-
-h_bs = data["base_station_height"]
+# Sacamos la altura de la estación base menos la altura de la posición del móvil
+h_bs = data["base_station_height"] - data["mobile_height"]
 
 for movil in data["mobiles"]:
-        d_rx = movil["distance"]
+        d_rx = movil["distance_x"]
         d_total = round(np.sqrt(np.power(d_rx, 2) + np.power(h_bs, 2)), 3)
         movil["real_distance"] = d_total
-        print(f"Distancia real entre antenas: {d_total}")
-
-# Guarda el archivo actualizado
-with open("datos.json", 'w') as f:
-    json.dump(data, f, indent=2)
+        print(f'"real_distance": {d_total},')
+# En el print anterior obtebemos la distancia entre la BS y cada móvil (hipotenusa de un triangulo escaleno)
